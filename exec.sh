6a61c40313cc3d781c274a02f4cf1ec0c532fcae
idjelaili <imadeddinedjelaili@gmail.com>
2021-09-20 20:56:59 +0200

    test

--- exec.sh
+++ exec.sh
@@ -1,3 +1,3 @@
-aws s3 cp layer.zip s3://selinium-test-bucket/src/SeleniumChromiumLayer.zip
+aws s3 cp layer.zip s3://selinium-test-bucket/src/SeleniumChromiumLayer.zip 
 aws s3 cp deploy.zip s3://selinium-test-bucket/src/ScreenshotFunction.zip
-aws cloudformation create-stack --stack-name LambdaScreenshot --template-body file://cloud.yaml --parameters ParameterKey=BucketName,ParameterValue=selinium-test-bucket --capabilities CAPABILITY_IAM
+aws cloudformation create-stack --stack-name LambdaScreenshot --template-body file://cloud.yaml --parameters ParameterKey=BucketName,ParameterValue=selinium-test-bucket --capabilities CAPABILITY_IAM --region eu-west-1
